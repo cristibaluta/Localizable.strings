777068059677a14b520a81c750326b7362c46d13
Baluta Cristian <cristi.baluta@gmail.com>
2015-10-14 09:20:21 +0300

    viper

---
+++ Localizabler/Wireframe.swift
@@ -0,0 +1,13 @@
+//
+//  Wireframe.swift
+//  Localizabler
+//
+//  Created by Baluta Cristian on 14/10/15.
+//  Copyright © 2015 Cristian Baluta. All rights reserved.
+//
+
+import Cocoa
+
+class Wireframe: NSObject {
+
+}
